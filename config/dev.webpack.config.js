16bd154d5b4c9e614309c4676b326ec3b9b68bb9
Ryan Long <rlong@redhat.com>
2021-03-25 09:18:20 -0400

    add build pipeline (#2)
    * add build pipeline
    
    * fix lint
    
    * use new key
    
    * update deploy repo

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
@@ -5,15 +6,18 @@ const { config: webpackConfig, plugins } = config({
   debug: true,
   https: true,
   useFileHash: false,
+  modules: ['subscriptionCentral'],
   ...(process.env.BETA && { deployment: 'beta/apps' })
 });
 
 plugins.push(
   require('@redhat-cloud-services/frontend-components-config/federated-modules')({
     root: resolve(__dirname, '../'),
+    moduleName: 'subscriptionCentral',
     useFileHash: false
   })
 );
+
 module.exports = {
   ...webpackConfig,
   plugins
--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,14 +1,18 @@
+/* eslint-disable */
+
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
+  modules: ['subscriptionCentral'],
   ...(process.env.BETA && { deployment: 'beta/apps' })
 });
 
 plugins.push(
   require('@redhat-cloud-services/frontend-components-config/federated-modules')({
-    root: resolve(__dirname, '../')
+    root: resolve(__dirname, '../'),
+    moduleName: 'subscriptionCentral'
   })
 );
 
