90339a705de62021b024e3ff86e4431db39978a7
Joel Myers <joeldmyers@gmail.com>
2021-04-06 15:19:38 -0400

    light refactor of platformServices test

--- src/utilities/__tests__/platformServices.test.ts
+++ src/utilities/__tests__/platformServices.test.ts
@@ -1,27 +1,33 @@
 import { authenticateUser } from '../platformServices';
 
+beforeEach(() => {
+  jest.clearAllMocks();
+  window.insights = {
+    chrome: {
+      auth: {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        getUser: () => {}
+      }
+    }
+  };
+});
+
 describe('Authenticate User method', () => {
   it('should return a promise with user data', () => {
-    window.insights = {
-      chrome: {
-        auth: {
-          getUser: jest.fn().mockImplementation(
-            () =>
-              new Promise((resolve) => {
-                process.nextTick(() => {
-                  resolve({
-                    identity: {
-                      user: {
-                        email: 'john.doe@redhat.com'
-                      }
-                    }
-                  });
-                });
-              })
-          )
-        }
-      }
-    };
+    window.insights.chrome.auth.getUser = jest.fn().mockImplementation(
+      () =>
+        new Promise((resolve) => {
+          process.nextTick(() => {
+            resolve({
+              identity: {
+                user: {
+                  email: 'john.doe@redhat.com'
+                }
+              }
+            });
+          });
+        })
+    );
 
     expect(authenticateUser()).resolves.toEqual({
       identity: {
@@ -33,21 +39,16 @@ describe('Authenticate User method', () => {
   });
 
   it('should throw an error if rejected', () => {
-    window.insights = {
-      chrome: {
-        getUser: jest.fn().mockImplementation(
-          () =>
-            new Promise((resolve, reject) => {
-              process.nextTick(() => {
-                reject({
-                  error: 'Error getting user'
-                });
-              });
-            })
-        )
-      }
-    };
-
-    expect(() => authenticateUser()).toThrow(Error);
+    window.insights.chrome.auth.getUser = jest.fn().mockImplementation(
+      () =>
+        new Promise((resolve, reject) => {
+          process.nextTick(() => {
+            reject({
+              error: 'Error getting user'
+            });
+          });
+        })
+    );
+    expect(authenticateUser()).rejects.toEqual({ error: 'Error getting user' });
   });
 });
