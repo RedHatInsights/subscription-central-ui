e409c3ae02d95e73b71f5de085ec86b390001190
Joel Myers <joeldmyers@gmail.com>
2021-04-07 12:37:09 -0400

    creating EmptyTable component for better reusability

--- src/components/emptyState/index.js
+++ src/components/emptyState/index.js
@@ -1,6 +1,7 @@
 import CreateManifestPanel from './CreateManifestPanel';
+import EmptyTable from './EmptyTable';
 import NoManifestsFound from './NoManifestsFound';
 import NoSearchResults from './NoSearchResults';
 import Processing from './Processing';
 
-export { CreateManifestPanel, NoManifestsFound, NoSearchResults, Processing };
+export { CreateManifestPanel, EmptyTable, NoManifestsFound, NoSearchResults, Processing };
