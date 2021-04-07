35161367716f2c3bf25320a202daa1a27fb75a2f
Joel Myers <joeldmyers@gmail.com>
2021-04-07 11:15:44 -0400

    cleanup of platform services test

--- src/utilities/__tests__/platformServices.test.ts
+++ src/utilities/__tests__/platformServices.test.ts
@@ -14,20 +14,13 @@ beforeEach(() => {
 
 describe('Authenticate User method', () => {
   it('should return a promise with user data', () => {
-    window.insights.chrome.auth.getUser = jest.fn().mockImplementation(
-      () =>
-        new Promise((resolve) => {
-          process.nextTick(() => {
-            resolve({
-              identity: {
-                user: {
-                  email: 'john.doe@redhat.com'
-                }
-              }
-            });
-          });
-        })
-    );
+    window.insights.chrome.auth.getUser = jest.fn().mockResolvedValue({
+      identity: {
+        user: {
+          email: 'john.doe@redhat.com'
+        }
+      }
+    });
 
     expect(authenticateUser()).resolves.toEqual({
       identity: {
@@ -39,16 +32,10 @@ describe('Authenticate User method', () => {
   });
 
   it('should throw an error if rejected', () => {
-    window.insights.chrome.auth.getUser = jest.fn().mockImplementation(
-      () =>
-        new Promise((resolve, reject) => {
-          process.nextTick(() => {
-            reject({
-              error: 'Error getting user'
-            });
-          });
-        })
-    );
+    window.insights.chrome.auth.getUser = jest.fn().mockRejectedValue({
+      error: 'Error getting user'
+    });
+
     expect(authenticateUser()).rejects.toEqual({ error: 'Error getting user' });
   });
 });
