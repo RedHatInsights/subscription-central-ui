50d545b4e3410ff6228b9fb879f3094cf6c3413f
Joel Myers <joeldmyers@gmail.com>
2021-04-13 10:03:09 -0400

    proxy config setup on beta local to avoid CORS issue

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -6,8 +6,22 @@ const { config: webpackConfig, plugins } = config({
   debug: true,
   https: true,
   useFileHash: false,
+
   modules: ['manifests'],
-  ...(process.env.BETA && { deployment: 'beta/apps' })
+  ...(process.env.BETA && {
+    deployment: 'beta/apps',
+    useProxy: true,
+    customProxy: [
+      {
+        context: (path) => path.includes('/api/'),
+        target: 'https://api.access.qa.redhat.com',
+        secure: true,
+        changeOrigin: true,
+        autoRewrite: true,
+        ws: true
+      }
+    ]
+  })
 });
 
 plugins.push(
