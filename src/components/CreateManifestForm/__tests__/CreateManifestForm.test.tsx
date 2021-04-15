88e2c4dc47a4644084a8def0d9fcb550aa57adf0
Joel Myers <joeldmyers@gmail.com>
2021-04-15 14:12:51 -0400

    wip tests

--- src/components/CreateManifestForm/__tests__/CreateManifestForm.test.tsx
+++ src/components/CreateManifestForm/__tests__/CreateManifestForm.test.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import CreateManifestFormError from '../CreateManifestFormError';
+import CreateManifestForm from '../CreateManifestForm';
+import { SatelliteVersion } from '../../../hooks/useSatelliteVersions';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
-it('renders correctly', () => {
-  const { container } = render(<CreateManifestFormError />);
-  expect(container).toMatchSnapshot();
+const queryClient = new QueryClient();
+
+describe('Create Manifest Form', () => {
+  it('renders correctly', () => {
+    const satelliteVersions: SatelliteVersion[] = [
+      { description: 'Satellite v6.2', value: 'sat-6.2' }
+    ];
+
+    const props = { satelliteVersions, handleModalToggle: () => 'foo', isModalOpen: true };
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <CreateManifestForm {...props} />
+      </QueryClientProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
