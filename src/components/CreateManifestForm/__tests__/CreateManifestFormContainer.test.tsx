1609f1eaaebf1d28e368d3a8548e45eac78c5d02
Joel Myers <joeldmyers@gmail.com>
2021-04-16 12:38:02 -0400

    updating Create Manifest Form Container

--- src/components/CreateManifestForm/__tests__/CreateManifestFormContainer.test.tsx
+++ src/components/CreateManifestForm/__tests__/CreateManifestFormContainer.test.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import CreateManifestFormContainer from '../CreateManifestFormContainer';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { render } from '@testing-library/react';
+import useSatelliteVersions, { SatelliteVersion } from '../../../hooks/useSatelliteVersions';
+
+jest.mock('../../../hooks/useSatelliteVersions');
+
 const queryClient = new QueryClient();
 
-describe('Create Manifest Form container', () => {
-  it('renders loading when data is loading', () => {
+describe('Create Manifest Form Container', () => {
+  it('renders correctly once satellite versions have been loaded', () => {
+    (useSatelliteVersions as jest.Mock).mockReturnValue({
+      body: [] as SatelliteVersion[]
+    });
     const props = { handleModalToggle: () => 'foo' };
     const { container } = render(
       <QueryClientProvider client={queryClient}>
