cd03b219639e3bcd4190d61f8eea0e6ac5a7eed4
Joel Myers <joeldmyers@gmail.com>
2021-04-16 13:39:01 -0400

    fix

--- src/components/CreateManifestForm/__tests__/CreateManifestFormLoading.test.tsx
+++ src/components/CreateManifestForm/__tests__/CreateManifestFormLoading.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import CreateManifestFormLoading from '../CreateManifestFormLoading';
 
-describe('Create Manifest Form Error Message', () => {
+describe('Create Manifest Form Loading Message', () => {
   it('renders correctly', () => {
     const { container } = render(<CreateManifestFormLoading title="Loading..." />);
     expect(container).toMatchSnapshot();
