a08dd0853d53b6f9b59ac6bbf0daa1ebc3db5414
Joel <joeldmyers@gmail.com>
2021-05-05 19:19:39 +0200

    fix to prevent entitlement list headers from being truncated

--- src/components/ManifestEntitlementsList/ManifestEntitlementsList.tsx
+++ src/components/ManifestEntitlementsList/ManifestEntitlementsList.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Table, TableHeader, TableBody } from '@patternfly/react-table';
+import { Table, TableHeader, TableBody, nowrap } from '@patternfly/react-table';
 import { Processing } from '../emptyState';
 import './ManifestEntitlementsList.scss';
 import { EntitlementsAttachedData, ManifestEntitlement } from '../../hooks/useManifestEntitlements';
@@ -18,12 +18,30 @@ const ManifestEntitlementsList: FC<ManifestEntitlementsListProps> = ({
   isError
 }) => {
   const columns = [
-    'Subscription name',
-    'SKU',
-    'Contract number',
-    'Quantity',
-    'Start date',
-    'End date'
+    {
+      title: 'Subscription name',
+      transforms: [nowrap]
+    },
+    {
+      title: 'SKU',
+      transforms: [nowrap]
+    },
+    {
+      title: 'Contract number',
+      transforms: [nowrap]
+    },
+    {
+      title: 'Quantity',
+      transforms: [nowrap]
+    },
+    {
+      title: 'Start date',
+      transforms: [nowrap]
+    },
+    {
+      title: 'End date',
+      transforms: [nowrap]
+    }
   ];
 
   const getFormattedDate = (date: string) => {
