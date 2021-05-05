0d3f78802c044fd6a674ff36e23f9abfa93a208c
Joel Myers <joeldmyers@gmail.com>
2021-05-05 19:54:10 +0200

    TEAMNADO-1994 Toggle List of attached Entitlements (#14)
    * WIP adding manifest details
    
    * minor code cleanup
    
    * adding adjusted index to fix sorting issue with collapsible table component from Patternfly
    
    * removing eslint change
    
    * minor cleanup of rowExpandedStatus state
    
    * updating existing snapshots to reflect adjustment from making rows expandable
    
    * adding tests and slight tweaks to css and entitlement date
    
    * removing a few unneeded data points in Manifest Entitlemnts Container test
    
    * renaming test files
    
    * adding type annotations to ManifestEntitlements
    
    * minor css cleanup
    
    * tweaks to manifest entitlement types
    
    * renaming type
    
    * minor tweak
    
    * renaming test description
    
    * small tweaks to resolve notes
    
    * fix to prevent entitlement list headers from being truncated
    
    * updating useManifestEntitlements to use new environment-specific RHSM API Base

---
+++ src/hooks/__tests__/useManifestEntitlements.test.tsx
@@ -0,0 +1,59 @@
+import React, { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import useManifestEntitlements from '../useManifestEntitlements';
+import { renderHook } from '@testing-library/react-hooks';
+import fetch, { enableFetchMocks } from 'jest-fetch-mock';
+
+enableFetchMocks();
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  fetch.resetMocks();
+});
+
+const queryClient = new QueryClient();
+
+interface wrapperProps {
+  children: ReactNode;
+}
+
+const wrapper = ({ children }: wrapperProps) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+describe('useManifestEntitlements hook', () => {
+  it('passes versions back when fetching from API', async () => {
+    const mockResponse = {
+      uuid: 'uuid-12345',
+      name: 'foo',
+      type: 'Satellite',
+      version: '6.9',
+      createdDate: '2020-00-19T17:17:10.000Z',
+      createdBy: 'john.doe',
+      lastModified: '2020-00-19T17:17:10.000Z',
+      entitlementsAttachedQuantity: 10,
+      entitlementsAttached: {
+        valid: true,
+        value: [
+          {
+            id: '123abc',
+            sku: 'SKU123',
+            contractNumber: '1123432',
+            entitlementQuantity: 100
+          }
+        ]
+      },
+      contentAccessMode: 'Entitlement Access'
+    };
+
+    fetch.mockResponseOnce(JSON.stringify(mockResponse));
+
+    const { result, waitFor } = renderHook(() => useManifestEntitlements('uuid-12345'), {
+      wrapper
+    });
+
+    await waitFor(() => result.current.isSuccess);
+
+    expect(result.current.data).toEqual(mockResponse);
+  });
+});
---
+++ src/hooks/useManifestEntitlements.ts
@@ -0,0 +1,54 @@
+import { useQuery, QueryObserverResult } from 'react-query';
+import Cookies from 'js-cookie';
+import { getConfig } from '../utilities/platformServices';
+
+interface ManifestEntitlement {
+  id: string;
+  sku: string;
+  contractNumber: string;
+  entitlementQuantity: number;
+  startDate?: string;
+  endDate?: string;
+  subscriptionName?: string;
+}
+
+interface ManifestEntitlementsData {
+  body: {
+    contentAccessMode: string;
+    createdBy: string;
+    createdDate: string;
+    entitlementsAttached: EntitlementsAttachedData;
+  };
+}
+
+interface EntitlementsAttachedData {
+  valid: boolean;
+  reason?: string;
+  value?: ManifestEntitlement[];
+}
+
+const getManifestEntitlements = (uuid: string): Promise<ManifestEntitlementsData> => {
+  const cs_jwt = Cookies.get('cs_jwt');
+  const { rhsmAPIBase } = getConfig();
+  return fetch(`${rhsmAPIBase}/management/v1/allocations/${uuid}?include=entitlements`, {
+    headers: { Authorization: `Bearer ${cs_jwt}` },
+    mode: 'cors'
+  })
+    .then((response) => response.json())
+    .catch((e) => {
+      console.error('Error fetching Manifest Entitlements data', e);
+    });
+};
+
+const useManifestEntitlements = (
+  uuid: string
+): QueryObserverResult<ManifestEntitlementsData, unknown> => {
+  return useQuery<any, Error>(['manifestEntitlements', uuid], () => getManifestEntitlements(uuid));
+};
+
+export {
+  EntitlementsAttachedData,
+  ManifestEntitlementsData,
+  ManifestEntitlement,
+  useManifestEntitlements as default
+};
