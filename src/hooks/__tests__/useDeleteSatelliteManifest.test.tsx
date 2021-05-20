5e22c7bfb018bc908404ea575421b98eaff0f608
Zane Woodfin <pwoodfin@redhat.com>
2021-05-20 11:04:44 -0400

    Add a failure test for the delete hook

--- src/hooks/__tests__/useDeleteSatelliteManifest.test.tsx
+++ src/hooks/__tests__/useDeleteSatelliteManifest.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useDeleteSatelliteManifest from '../useDeleteSatelliteManifest';
 import fetch, { enableFetchMocks } from 'jest-fetch-mock';
 
@@ -13,17 +13,48 @@ const wrapper = ({ children }: any) => {
 
 describe('useDeleteSatelliteManifest hook', () => {
   const { result, waitFor } = renderHook(() => useDeleteSatelliteManifest(), { wrapper });
+  let status: number;
 
   beforeEach(() => {
     //jest.clearAllMocks();
-    fetch.mockResponseOnce(JSON.stringify({}), { status: 204 });
+    fetch.mockResponseOnce(JSON.stringify({}), { status: status });
     queryClient.setQueryData('manifests', [{ uuid: '00000000-0000-0000-0000-000000000000' }]);
     result.current.mutate('00000000-0000-0000-0000-000000000000');
   });
 
-  it('it deletes the data locally', async () => {
-    await waitFor(() => result.current.isSuccess);
-    const manifests: Array<any> = queryClient.getQueryData('manifests');
-    expect(manifests.length).toEqual(0);
+  describe('when API returns a 204 status', () => {
+    beforeAll(() => {
+      status = 204;
+    });
+
+    it('deletes the data locally', async () => {
+      await waitFor(() => result.current.isSuccess);
+      const manifests: Array<any> = queryClient.getQueryData('manifests');
+      expect(manifests.length).toEqual(0);
+    });
+  });
+
+  describe('when the API returns a non-204 status', () => {
+    const originalConsoleError: () => void = console.error;
+
+    beforeAll(() => {
+      status = 404;
+    });
+
+    beforeEach(() => {
+      // Disabling console.error to prevent test suite failure for an expected error
+      console.error = jest.fn();
+    });
+
+    afterEach(() => {
+      console.error = originalConsoleError;
+    });
+
+    it('does not delete the data locally', async () => {
+      // We should be waiting for isError here, but there seems to be a bug in the testing library
+      await waitFor(() => result.current.isSuccess);
+      const manifests: Array<any> = queryClient.getQueryData('manifests');
+      expect(manifests.length).toEqual(1);
+    });
   });
 });
