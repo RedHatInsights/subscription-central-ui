e442bb861e6ca72bcd9a8dde6623ac60f9417168
Joel <joeldmyers@gmail.com>
2021-06-04 16:01:52 +0200

    working test for useUserPermissions

--- src/hooks/__tests__/useUserPermissions.test.tsx
+++ src/hooks/__tests__/useUserPermissions.test.tsx
@@ -1,19 +1,24 @@
-import useUserPermissions from '../useUserPermissions';
-import * as PlatformServices from '../../utilities/platformServices';
+import React, { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { renderHook } from '@testing-library/react-hooks';
 import fetch, { enableFetchMocks } from 'jest-fetch-mock';
+import { authenticateUser } from '../../utilities/platformServices';
+import useUserPermissions from '../useUserPermissions';
 import { createQueryWrapper } from '../../utilities/testHelpers';
-
-jest.mock('../../utilities/platformServices');
 
 enableFetchMocks();
 
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+jest.mock('../../utilities/platformServices', () => ({
+  ...jest.requireActual('../../utilities/platformServices'),
+  authenticateUser: jest.fn()
+}));
+
 describe('useUserPermissions hook', () => {
   it('gets the user permissions back from two API calls', async () => {
-    // const mockResponse = { body: [{ value: 'sat-6.9', description: 'Satellite 6.9' }] };
-    // fetch.mockResponseOnce(JSON.stringify(mockResponse));
-    const { authenticateUser } = PlatformServices;
-
     (authenticateUser as jest.Mock).mockResolvedValue({
       identity: {
         user: {
@@ -22,20 +27,27 @@ describe('useUserPermissions hook', () => {
       }
     });
 
+    const mockSCAStatusResponse = {
+      body: {
+        id: '123456',
+        simpleContentAccess: 'enabled',
+        simpleContentAccessCapable: true
+      }
+    };
+    fetch.mockResponseOnce(JSON.stringify(mockSCAStatusResponse));
+
     const { result, waitFor } = renderHook(() => useUserPermissions(), {
       wrapper: createQueryWrapper()
     });
-
     await waitFor(() => result.current.isSuccess);
 
     expect(result.current.data).toEqual({ isOrgAdmin: true, isSCACapable: true });
   });
 
-  it('does not return anything if the API call fails', async () => {
+  it('does not return anything and sets react-query status as isError if the Authenticate User API call fails', async () => {
     const originalError = console.error;
     console.error = jest.fn();
 
-    const { authenticateUser } = PlatformServices;
     (authenticateUser as jest.Mock).mockRejectedValue({ status: 'error' });
 
     const { result, waitFor } = renderHook(() => useUserPermissions(), {
