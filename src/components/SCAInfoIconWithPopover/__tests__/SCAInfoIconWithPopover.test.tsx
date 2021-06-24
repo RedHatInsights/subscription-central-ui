6c7c1a6eabcc8eeac59202c454edf0d1c6e4a7b0
Joel <joeldmyers@gmail.com>
2021-06-24 14:05:26 -0400

    Updated SCA Info Test

---
+++ src/components/SCAInfoIconWithPopover/__tests__/SCAInfoIconWithPopover.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { fireEvent, waitFor, render } from '@testing-library/react';
+import SCAInfoIconWithPopover from '..';
+
+describe('SCA Info Icon With Popover', () => {
+  it('stops event propagation on click', async () => {
+    const testFunctionShouldNotFire = jest.fn();
+
+    const { getByTestId } = render(
+      <div data-testid="bubble-test-wrapper" onClick={testFunctionShouldNotFire}>
+        <SCAInfoIconWithPopover />
+      </div>
+    );
+
+    fireEvent.click(getByTestId('sca-more-info-icon'));
+    await waitFor(() => {
+      expect(getByTestId('sca-more-info-paragraph')).toBeDefined();
+    });
+
+    expect(testFunctionShouldNotFire).toHaveBeenCalledTimes(0);
+  });
+});
