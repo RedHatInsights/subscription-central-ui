616e5757bdf634e6d5b3339db70c5f2a579dc82d
Joel <joeldmyers@gmail.com>
2021-06-30 15:03:24 -0400

    updating test to remove testid

--- src/components/SCAInfoIconWithPopover/__tests__/SCAInfoIconWithPopover.test.tsx
+++ src/components/SCAInfoIconWithPopover/__tests__/SCAInfoIconWithPopover.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, waitFor, render } from '@testing-library/react';
+import { fireEvent, waitFor, render, screen } from '@testing-library/react';
 import SCAInfoIconWithPopover from '..';
 
 describe('SCA Info Icon With Popover', () => {
@@ -14,7 +14,11 @@ describe('SCA Info Icon With Popover', () => {
 
     fireEvent.click(getByTestId('sca-more-info-icon'));
     await waitFor(() => {
-      expect(getByTestId('sca-more-info-paragraph')).toBeDefined();
+      expect(
+        screen.queryByText(
+          'Simple content access is a set of capabilities that enables a change in the way Red Hat manages its subscription and entitlement enforcement model. When enabled, you are allowed to consume content on your systems without strict entitlement enforcement.'
+        )
+      ).toBeDefined();
     });
 
     expect(testFunctionShouldNotFire).toHaveBeenCalledTimes(0);
