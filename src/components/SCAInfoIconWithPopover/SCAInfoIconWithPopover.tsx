202fffb989fa717a54ac29d6f579faee596ddafd
Joel <joeldmyers@gmail.com>
2021-06-30 15:03:49 -0400

    removing testid on paragraph in sca info icon with popover

--- src/components/SCAInfoIconWithPopover/SCAInfoIconWithPopover.tsx
+++ src/components/SCAInfoIconWithPopover/SCAInfoIconWithPopover.tsx
@@ -16,7 +16,7 @@ const SCAInfoIconWithPopover: FunctionComponent = () => {
         aria-label="Learn more about Simple Content Access"
         headerContent="Simple Content Access"
         bodyContent={
-          <p data-testid="sca-more-info-paragraph">
+          <p>
             Simple content access is a set of capabilities that enables a change in the way Red Hat
             manages its subscription and entitlement enforcement model. When enabled, you are
             allowed to consume content on your systems without strict entitlement enforcement.
