71c8109590a2791bc5fa39d2cea97834b0f53250
Joel <joeldmyers@gmail.com>
2021-06-30 15:31:14 -0400

    removing redundant test, removing trailing spaces from getBaseName test names

--- src/utilities/__tests__/getBaseName.test.ts
+++ src/utilities/__tests__/getBaseName.test.ts
@@ -1,19 +1,14 @@
 import { getBaseName } from '..';
 
 describe('utilities/getBaseName', () => {
-  it('should find the right base name on Stable ', () => {
+  it('should find the right base name on Stable', () => {
     expect(getBaseName('/insights/foo/bar/baz')).toEqual('/insights/foo');
     expect(getBaseName('/rhcs/bar/bar/baz')).toEqual('/rhcs/bar');
   });
 
-  it('should find the right base name on Beta ', () => {
+  it('should find the right base name on Beta', () => {
     expect(getBaseName('/beta/insights/foo/bar/baz')).toEqual('/beta/insights/foo');
     expect(getBaseName('/beta/test/fff/bar/baz')).toEqual('/beta/test/fff');
-  });
-
-  it('should find the right base name not on Beta ', () => {
-    expect(getBaseName('/insights/foo/bar/baz')).toEqual('/insights/foo');
-    expect(getBaseName('/test/fff/bar/baz')).toEqual('/test/fff');
   });
 
   it('should find the right base name not on Beta with a shorter url', () => {
