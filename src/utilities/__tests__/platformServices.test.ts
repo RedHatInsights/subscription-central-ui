babbefcf7142363118a2e61612b0827897111f93
Zane Woodfin <pwoodfin@redhat.com>
2021-11-03 11:20:20 -0400

    Increase test coverage to required thresholds

--- src/utilities/__tests__/platformServices.test.ts
+++ src/utilities/__tests__/platformServices.test.ts
@@ -1,4 +1,4 @@
-import { authenticateUser } from '../platformServices';
+import { authenticateUser, getEnvironment, getUserRbacPermissions } from '../platformServices';
 
 beforeEach(() => {
   window.insights = {
@@ -11,7 +11,7 @@ beforeEach(() => {
   };
 });
 
-describe('Authenticate User method', () => {
+describe('authenticateUser', () => {
   it('should return a promise with user data', () => {
     window.insights.chrome.auth.getUser = jest.fn().mockResolvedValue({
       identity: {
@@ -40,5 +40,45 @@ describe('Authenticate User method', () => {
     } catch (e) {
       expect(e.message).toEqual('Error authenticating user: Error getting user');
     }
+  });
+});
+
+describe('getUserRbacPermissions', () => {
+  it('should return a promise with user RBAC permissions', () => {
+    window.insights.chrome.getUserPermissions = jest.fn().mockResolvedValue([
+      { resourceDefinitions: [], permission: 'subscriptions:manifests:read' },
+      { resourceDefinitions: [], permission: 'subscriptions:manifests:write' }
+    ]);
+
+    expect(getUserRbacPermissions()).resolves.toEqual([
+      { resourceDefinitions: [], permission: 'subscriptions:manifests:read' },
+      { resourceDefinitions: [], permission: 'subscriptions:manifests:write' }
+    ]);
+  });
+
+  it('should throw an error if rejected', () => {
+    window.insights.chrome.getUserPermissions = jest.fn().mockImplementation(() => {
+      throw new Error('Nope');
+    });
+
+    try {
+      authenticateUser();
+    } catch (e) {
+      expect(e.message).toEqual('Error getting user permissions: Nope');
+    }
+  });
+});
+
+describe('getEnvironment', () => {
+  it('returns the environment', () => {
+    window.insights.chrome.getEnvironment = jest.fn().mockReturnValue('qa');
+
+    expect(getEnvironment()).toEqual('qa');
+  });
+
+  it('defaults to returning "ci"', () => {
+    window.insights.chrome.getEnvironment = jest.fn().mockReturnValue(null);
+
+    expect(getEnvironment()).toEqual('ci');
   });
 });
