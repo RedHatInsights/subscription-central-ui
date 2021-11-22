fe175c1f8184060a14aa9378d5226ca967682f20
Dmitrii Malyshev <dmalyshe@dmalyshe.remote.csb>
2021-11-22 17:04:20 -0500

    Add learn more link to manifests

--- src/pages/SatelliteManifestPage/SatelliteManifestPage.tsx
+++ src/pages/SatelliteManifestPage/SatelliteManifestPage.tsx
@@ -9,18 +9,27 @@ import Unavailable from '@redhat-cloud-services/frontend-components/Unavailable'
 import { Processing } from '../../components/emptyState';
 import { useQueryClient } from 'react-query';
 import { User } from '../../hooks/useUser';
+import ExternalLink from '../../components/ExternalLink';
 
 const SatelliteManifestPage: FC = () => {
   const { isLoading, isFetching, error, data } = useSatelliteManifests();
 
   const queryClient = useQueryClient();
   const user: User = queryClient.getQueryData('user');
+  const manifestsMoreInfoLink =
+    'https://access.redhat.com/documentation/en-us/subscription_central/2021/html/' +
+    'creating_and_managing_manifests_for_a_connected_satellite_server/index';
 
   return (
     <>
       <PageHeader>
         <PageHeaderTitle title="Manifests" />
-        <p>Export subscriptions to your on-premise subscription management application</p>
+        <p>
+          Export subscriptions to your on-premise subscription management application.{' '}
+          <ExternalLink href={manifestsMoreInfoLink}>
+            Learn more about creating and managing manifests for a connected Satellite Server
+          </ExternalLink>
+        </p>
       </PageHeader>
       <Main>
         <>
