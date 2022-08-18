ee437446a9790b68d1d9e63b81fbba8e932c67ec
Luke Green <lgreen@redhat.com>
2022-08-18 10:46:18 -0500

    Replace all direct cookie accesses with the helper

--- src/hooks/useDeleteSatelliteManifest.ts
+++ src/hooks/useDeleteSatelliteManifest.ts
@@ -1,9 +1,8 @@
-import Cookies from 'js-cookie';
 import { useMutation, useQueryClient } from 'react-query';
 import { ManifestEntry } from './useSatelliteManifests';
 
-const deleteSatelliteManifest = (uuid: string) => {
-  const jwtToken = Cookies.get('cs_jwt');
+const deleteSatelliteManifest = async (uuid: string) => {
+  const jwtToken = await window.insights.chrome.auth.getToken();
   return fetch(`/api/rhsm/v2/manifests/${uuid}?force=true`, {
     method: 'DELETE',
     headers: { Authorization: `Bearer ${jwtToken}` }
