ebf35773d9da1bc5dd4373cb5642edde39872953
Brooklyn Lower <blower@redhat.com>
2022-09-08 13:17:40 -0400

    "Disable create manifest button, blank state page

--- src/components/CreateManifestButtonWithModal/__tests__/CreateManifestButtonWithModal.test.tsx
+++ src/components/CreateManifestButtonWithModal/__tests__/CreateManifestButtonWithModal.test.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import React, { FC } from 'react';
+import { fireEvent, getByText, render, screen, waitFor } from '@testing-library/react';
 import CreateManifestButtonWithModal from '..';
 import { createQueryWrapper } from '../../../utilities/testHelpers';
 import useSatelliteVersions, { SatelliteVersion } from '../../../hooks/useSatelliteVersions';
-
-jest.mock('../../../hooks/useSatelliteVersions');
+import { User } from '../../../hooks/useUser';
+import { CreateManifestPanel } from '../../emptyState';
+import { ExitStatus } from 'typescript';
+import SatelliteManifestPanel from '../../SatelliteManifestPanel';
+import { mergeProps } from '@patternfly/react-table/dist/esm/components/Table/base';
+import { QueryClientProvider, useQueryClient } from 'react-query';
 
 describe('Create Manifest Button With Modal', () => {
   it('renders the modal properly when the button is clicked', async () => {
-    (useSatelliteVersions as jest.Mock).mockReturnValue({
+    jest.mock('../../../hooks/useSatelliteVersions', () => ({
       body: [] as SatelliteVersion[],
-      isError: false,
-      isLoading: false
-    });
-
-    const { getByText } = render(<CreateManifestButtonWithModal />, {
-      wrapper: createQueryWrapper()
-    });
+      isLoading: false,
+      isError: false
+    }));
+    const { getByText } = render(<CreateManifestButtonWithModal {...get('props')} />);
 
     fireEvent.click(getByText('Create new manifest'));
     await screen.findAllByText(
       'Creating a new manifest allows you to export subscriptions to your on-premise subscription management application.'
     );
+    // waitFor(() => expect(screen.findByText('Create Manifest')).toBeInTheDocument());
     /**
      * document.body needed because the modal
      * does not render within the container
