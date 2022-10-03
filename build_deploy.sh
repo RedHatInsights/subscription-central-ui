d8fdd29d4ddd3a8ee68cec554703178385dc69bf
Reed Cowan <reedcowan30@gmail.com>
2022-10-03 14:02:58 -0400

    fixed component path

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,7 +4,7 @@
 # Export vars for helper scripts to use
 # --------------------------------------------
 # name of app-sre "application" folder this component lives in; needs to match for quay
-export COMPONENT="rhsm"
+export COMPONENT="rhsm-api-proxy"
 # IMAGE should match the quay repo set by app.yaml in app-interface
 export IMAGE="quay.io/cloudservices/subscription-central-ui"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
