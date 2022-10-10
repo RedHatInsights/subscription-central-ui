6784e52e09e22c30ccdeb39f6be25de1286407d6
Brooklyn Lower <blower@redhat.com>
2022-10-10 14:38:31 -0400

    Removal of ErrorMessge Comp, Unavail "

--- src/components/emptyState/index.js
+++ src/components/emptyState/index.js
@@ -1,7 +1,6 @@
 import CreateManifestPanel from './CreateManifestPanel';
-import ErrorMessage from './ErrorMessage';
 import NoManifestsFound from './NoManifestsFound';
 import NoSearchResults from './NoSearchResults';
 import Processing from './Processing';
 
-export { CreateManifestPanel, ErrorMessage, NoManifestsFound, NoSearchResults, Processing };
+export { CreateManifestPanel, NoManifestsFound, NoSearchResults, Processing };
