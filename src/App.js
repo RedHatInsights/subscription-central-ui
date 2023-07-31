9fa8807e47be3dd04d3691304e6eb34722bce7f3
Brooklyn Lower <blower@redhat.com>
2023-07-31 16:31:19 -0400

    "Remove App_navigation event"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
 import { ManifestRoutes } from './Routes';
 import './App.scss';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import NotificationProvider from './contexts/NotificationProvider';
 import Notifications from './components/Notifications';
-import { useNavigate } from 'react-router-dom';
-import { getPartialRouteFromPath } from './utilities/routeHelpers';
+import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
+import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/Registry';
+import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,18 +21,12 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    insights.chrome.init();
+  const { updateDocumentTitle } = useChrome();
 
-    insights.chrome.identifyApp('manifests');
-    const unregister = insights.chrome.on('APP_NAVIGATION', (event) => {
-      const partialURL = getPartialRouteFromPath(event.domEvent.href);
-      navigate(partialURL);
-    });
-    return () => {
-      unregister();
-    };
+  useEffect(() => {
+    const registry = getRegistry();
+    registry.register({ notifications: notificationsReducer });
+    updateDocumentTitle('manifests');
   }, []);
 
   return (
@@ -45,4 +39,4 @@ const App = () => {
   );
 };
 
-export default connect()(App);
+export default App;
