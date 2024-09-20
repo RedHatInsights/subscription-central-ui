eb8f72e19d86f8c64ba74796ee4a786a0e068f38
(Jonathan) Luke Green <lgreen@redhat.com>
2024-09-20 15:07:09 -0500

    remove beta build (#229)

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -5,8 +5,7 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
-  modules: ['manifests'],
-  ...(process.env.BETA && { deployment: 'beta/apps' })
+  modules: ['manifests']
 });
 
 plugins.push(
